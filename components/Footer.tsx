15da66017261e6ff173d8be604be80e6529e1ba1
Daniel Merkl <daniel.merkl.dm@gmail.com>
2025-11-11 22:42:46 +0100

    feat: add links to Impressum and Datenschutz

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,1 +1,20 @@
-export const Footer = () => <footer>Footer</footer>;
+import Link from "next/link";
+
+export const Footer = () => (
+  <footer className="container flex gap-4 justify-center py-4 text-sm text-base-200">
+    <Link
+      className="hover:text-base-100 transition-colors"
+      href="https://www.rot-gold-casino.de/impressum"
+      target="_blank"
+    >
+      Impressum
+    </Link>
+    <Link
+      className="hover:text-base-100 transition-colors"
+      href="https://www.rot-gold-casino.de/datenschutzerklaerung"
+      target="_blank"
+    >
+      Datenschutz
+    </Link>
+  </footer>
+);
