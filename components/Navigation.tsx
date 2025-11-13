8cad89fe854a74d604d6a9bf908de5caa80fda38
Daniel Merkl <daniel.merkl.dm@gmail.com>
2025-11-13 18:15:03 +0100

    feat: update navigation link to prevent shrinking on smaller screens

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -45,7 +45,7 @@ export const Navigation = () => {
         </div>
         <Link
           href="/"
-          className="block w-fit ml-2 bg-secondary-700 text-sm hover:bg-secondary-600 px-3 py-1 rounded-full transition-colors font-bold cursor-pointer !text-white"
+          className="block shrink-0 w-fit ml-2 bg-secondary-700 text-sm hover:bg-secondary-600 px-3 py-1 rounded-full transition-colors font-bold cursor-pointer !text-white"
         >
           Tickets sichern
         </Link>
